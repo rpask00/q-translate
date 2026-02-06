ebf8710cb0a90b49cb5bfc89b27cd379f8a9b926
Rafal Pasko <rp.261000@gmail.com>
2026-02-06 22:42:15 +0100

    feat: Implement translation logic in main function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
-
+use q_translate::utils;
+use std::fs;
+use std::fs::File;
 use std::io::Write;
 
-
-
 #[tokio::main]
-async  fn main() -> std::io::Result<()> {
+async fn main() -> std::io::Result<()> {
+    let SOURCE_LANG = "pl";
+    let TARGET_LANG = "ua";
+
+    let source_json =
+        serde_json::from_str(&fs::read_to_string(format!("assets/{SOURCE_LANG}.json"))?)?;
+    let mut target_json =
+        serde_json::from_str(&fs::read_to_string(format!("assets/{TARGET_LANG}.json"))?)?;
+
+    utils::traverse(&source_json, &mut target_json, None, 0, TARGET_LANG).await;
+
+    let mut target_file = File::create("assets/ua.json")?;
+    target_file.write_all(serde_json::to_string_pretty(&target_json)?.as_bytes())?;
 
     Ok(())
 }
-
